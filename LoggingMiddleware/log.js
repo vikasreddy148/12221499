61c2912af3a50e00d8cb17a66ac59c2c59774d5e
vikasreddy148 <kathulavikasr@gmail.com>
2025-07-14 11:28:32 +0530

    feat: setup Vite + React frontend with logging

---
+++ LoggingMiddleware/log.js
@@ -0,0 +1,24 @@
+
+import axios from 'axios';
+
+export const log = async (stack, level, pkg, message, token) => {
+  try {
+    const res = await axios.post(
+      'http://20.244.56.144/evaluation-service/logs', 
+      {
+        stack,
+        level,
+        package: pkg,
+        message
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+    console.log('Log sent:', res.data.message);
+  } catch (err) {
+    console.error('Log failed:', err.response?.data || err.message);
+  }
+};
